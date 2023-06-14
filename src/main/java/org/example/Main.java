2a6f1d32a00301b9e63c1a5cfe8ce65642004dae
zhanq <zhanq@jiguang.cn>
2023-06-14 17:52:13 +0800

    commit message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main() {
-        System.out.printf("Hello and welcome 222");
+        System.out.printf("Hello and welcome4444");
     }
 }
